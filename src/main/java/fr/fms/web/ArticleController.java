2b1eff25ddfcb7297e38b66259ab02baffe1baec
Donovan Seulin <nel640@hotmail.fr>
2024-04-09 15:48:57 +0200

    page 21 masterclass

--- src/main/java/fr/fms/web/ArticleController.java
+++ src/main/java/fr/fms/web/ArticleController.java
@@ -1,6 +1,5 @@
 package fr.fms.web;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,7 +9,6 @@ import org.springframework.ui.Model;
 
 import fr.fms.dao.ArticleRepository;
 import fr.fms.entities.Article;
-import javassist.expr.NewArray;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,13 +21,22 @@ public class ArticleController
 	ArticleRepository articleRepository;
 	
 	@GetMapping("/index")
-	public String index(Model model , @RequestParam(name="page" , defaultValue = "0") int page) 
+	public String index(Model model , @RequestParam(name="page" , defaultValue = "0") int page,
+									  @RequestParam(name="keyword" , defaultValue = "") String kw) 
 	{
-		Page<Article> articles = articleRepository.findAll(PageRequest.of(page, 5));
+		Page<Article> articles = articleRepository.findByDescriptionContains(kw , PageRequest.of(page, 5));
+		//Page<Article> articles = articleRepository.findAll(PageRequest.of(page, 5));
 		model.addAttribute("listArticles" , articles.getContent());
 		model.addAttribute("pages", new int[articles.getTotalPages()]);
 		model.addAttribute("currentPage" , page);
+		model.addAttribute("keyword" , kw); 
 		return "articles" ;
+	}
+	@GetMapping("/delete")
+	public String delete(Long id , int page , String keyword)
+	{
+		articleRepository.deleteById(id);
+		return "redirect:/index?page="+page+"&keyword="+keyword;
 	}
 	
 }
